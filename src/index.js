cc39de6af17940da55d2aa9c8bf02fecbfd60454
Arthur <pigeon.arthur@hotmail.fr>
2021-03-14 11:15:50 +0100

    setup node BUG

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const { JSDOM } = require('jsdom');
-const { window } = new JSDOM('');
-const $ = require('jquery')(window);
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
 
 $(document).ready(() => {
-	var mymap = L.map('mapid').setView([46.227638, 2.213749], 6);
-	var p = document.getElementById('demo');
+	var mymap = L.map("mapid").setView([46.227638, 2.213749], 6);
+	var p = document.getElementById("demo");
 
 	console.log(mymap);
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
+	L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(mymap);
 	var control = L.Routing.control({
 		waypoints: [],
 		router: new L.Routing.osrmv1({
-			language: 'fr',
-			profile: 'car',
+			language: "fr",
+			profile: "car",
 		}),
 		geocoder: L.Control.Geocoder.nominatim({}),
 	}).addTo(mymap);
@@ -21,19 +21,13 @@ $(document).ready(() => {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(showPosition);
 		} else {
-			p.innerHTML = 'Geolocation non supportée par ce navigateur.';
+			p.innerHTML = "Geolocation non supportée par ce navigateur.";
 		}
 	}
 
 	function showPosition(position) {
-		mymap.setView(
-			[position.coords.latitude, position.coords.longitude],
-			15
-		);
-		var Moi = L.marker([
-			position.coords.latitude,
-			position.coords.longitude,
-		])
+		mymap.setView([position.coords.latitude, position.coords.longitude], 15);
+		var Moi = L.marker([position.coords.latitude, position.coords.longitude])
 			.bindPopup("<b>C'EST MOI !</b><br>")
 			.openPopup();
 		Moi.addTo(mymap);
@@ -41,27 +35,27 @@ $(document).ready(() => {
 		var result;
 		geocoder.reverse(Location, 1, result);
 		console.log(result);
-		p.innerHTML = 'Latitude: ' + position.coords.latitude;
-		p.innerHTML += '<br/>Longitude: ' + position.coords.longitude;
-		p.innerHTML += 'Plus ou Moins ' + position.coords.accuracy + ' mètres.';
+		p.innerHTML = "Latitude: " + position.coords.latitude;
+		p.innerHTML += "<br/>Longitude: " + position.coords.longitude;
+		p.innerHTML += "Plus ou Moins " + position.coords.accuracy + " mètres.";
 		//console.log("Plus ou Moins " + position.coords.accuracy + " mètres.");
 	}
 
 	const apitest = () => {
 		data = {
-			postal_codes: '75019',
+			postal_codes: "75019",
 			price_max: 500000,
 			area_max: 100,
 			limit: 4,
 		};
 
 		$.get(
-			'demo_test.txt',
+			"demo_test.txt",
 			data,
 			(result) => {
 				console.log(result);
 			},
-			'json'
+			"json"
 		);
 	};
 });
